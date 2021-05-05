59e96bb8e5d3c457a44a7e44ca60ef94ada70395
Rajnish Mishra <contact.rajnishmishra@gmail.com>
2021-05-05 17:20:16 +0530

    Added support for python 3.9, fixed runtime dependencies, fixed setup.py errors

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ DEPENDENCIES = []
 
 with open('requirements.txt') as requirements:
     for requirement in requirements.readlines():
-        if requirement.startswith('git+git://'):
+        if requirement.startswith('git+git://') or requirement.startswith('https://'):
             DEPENDENCIES.append(requirement)
         else:
             REQUIREMENTS.append(requirement)
@@ -69,7 +69,7 @@ setup(
     include_package_data=True,
     install_requires=REQUIREMENTS,
     dependency_links=DEPENDENCIES,
-    python_requires='>=3.4, <=3.8',
+    python_requires='>=3.4, <=3.10',
     license='BSD',
     zip_safe=True,
     platforms=['any'],
@@ -91,6 +91,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3 :: Only',
     ],
     test_suite='tests'
